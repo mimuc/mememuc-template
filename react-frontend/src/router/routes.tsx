758382ce8968e7e6db722b790d5ad8a4f1adbfa4
Simon Lund <simon.lund@campus.lmu.de>
2023-01-06 21:55:32 +0100

    :adhesive_bandage: remove app layout for now

--- react-frontend/src/router/routes.tsx
+++ react-frontend/src/router/routes.tsx
@@ -1,22 +1,23 @@
 import {Outlet, RouteObject} from "react-router-dom";
-import {AppLayout} from "src/layouts";
+// import {AppLayout} from "src/layouts";
 import {ErrorPage} from "./ErrorPage/ErrorPage";
 import {MemesPage, MemePage, NewPage, ProfilePage, StartPage} from "src/pages";
 
-const AppWrapper = () => {
-    return <AppLayout>
-        <Outlet/>
-    </AppLayout>
-}
+// const AppWrapper = () => {
+//     return <AppLayout>
+//         <Outlet/>
+//     </AppLayout>
+// }
+
 export const routes: RouteObject[] = [
     {
-        element: <AppWrapper/>,
+        element: <Outlet/>,//<AppWrapper/>,
         errorElement: <ErrorPage/>,
         path: '/',
         children: [
             {
                 index: true,
-                element: <StartPage/>
+                element: <NewPage/>
             },
             {
                 path: 'memes',
@@ -35,10 +36,6 @@ export const routes: RouteObject[] = [
                 path: 'profile',
                 element: <ProfilePage/>
             },
-            {
-                path: 'new',
-                element: <NewPage/>
-            }
         ]
     }
 ]
