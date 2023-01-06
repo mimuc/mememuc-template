4df2e54fcd5c14715021633d69d070f6f79e5688
Simon Lund <simon.lund@campus.lmu.de>
2023-01-06 21:17:51 +0100

    :label: add basic types Meme, Template, User

--- react-frontend/src/types/index.ts
+++ react-frontend/src/types/index.ts
@@ -1,3 +1,22 @@
 export type Template = {
+    id: string
+    name: string
+}
 
+export type Meme = {
+    id: string;
+    name: string;
+    image: string;
+    totalLikes: number;
+    totalComments: number;
+    createdAt: string;
+    liked: boolean;
+    creator?: {
+        id: string;
+        displayName: string;
+    }
+}
+
+export type User = {
+    name: string
 }
