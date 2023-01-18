e0be9b88390ab42792dbfb4e8cd4c779ca60b522
ragor114 <69634980+ragor114@users.noreply.github.com>
2023-01-18 22:17:42 +0100

    connected backend for registration

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import SignIn from './components/login/login.tsx';
+import { SignIn, Register } from './components/login/login.tsx';
 import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 import MemePage from './components/memePage/memePage.tsx';
 import ProfilePage from './components/profile/profilePage.tsx';
@@ -11,6 +11,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<SignIn></SignIn>}></Route>
+          <Route path='/register' element={<Register/>}></Route>
         <Route path="/memePage" element={<MemePage></MemePage>}></Route>
         <Route path="/profile" element={<ProfilePage/>}></Route>
       </Routes>
--- react-frontend/src/components/login/login.tsx
+++ react-frontend/src/components/login/login.tsx
@@ -19,7 +19,100 @@ import { Navigate, useNavigate } from "react-router-dom";
 
 const theme = createTheme();
 
-export default function SignIn() {
+function Register() {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      const data = new FormData(event.currentTarget);
+      console.log({
+        email: data.get('email'),
+        password: data.get('password'),
+      });
+      let bodyContent = JSON.stringify({
+        timestamp: Date.now(),
+        username: data.get('username'),
+        password: data.get('password'),
+        email: data.get('email'),
+      });
+      console.log(bodyContent);
+      console.log("trying to register");
+      fetch('http://localhost:3001/users/insert', {
+        method: 'POST',
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          "Content-Type": "application/json"
+        },
+        body: bodyContent,
+      }).then((res) => {
+        console.log(res.status);
+      });
+  };
+
+  return (<ThemeProvider theme={theme}>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+      >
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Username"
+              name="username"
+              autoFocus
+          />
+          <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+          />
+          <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+          />
+          <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+          >
+            Register
+          </Button>
+          <Grid container>
+            <Grid item>
+              <Link href="/" variant="body2">
+                {"You already have an account? Sign In"}
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
+  </ThemeProvider>);
+}
+
+
+function SignIn() {
   const navigate = useNavigate();
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -28,9 +121,23 @@ export default function SignIn() {
       email: data.get('email'),
       password: data.get('password'),
     });
+    fetch('http://localhost:3001/users/auth', {
+      method: 'POST',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        email: data.get('email'),
+        password: data.get('password'),
+      }),
+    }).then((res) => {
+      console.log(res);
+      console.log(res.status);
+    });
     //TODO: Schauen ob email und passwort vorhanden und richtig
     
-    navigate("/memePage");
+    //navigate("/memePage");
     //<Navigate to="/memePage"/>
   };
 
@@ -46,9 +153,6 @@ export default function SignIn() {
             alignItems: 'center',
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
@@ -82,13 +186,8 @@ export default function SignIn() {
               Sign In
             </Button>
             <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
               <Grid item>
-                <Link href="#" variant="body2">
+                <Link href="/register" variant="body2">
                   {"Don't have an account? Sign Up"}
                 </Link>
               </Grid>
@@ -99,3 +198,5 @@ export default function SignIn() {
     </ThemeProvider>
   );
 }
+
+export { SignIn, Register }
