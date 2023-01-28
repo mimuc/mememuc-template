09d51c148435ea1b59e8e3cad2e1c52db269a83f
Simon Lund <simon.lund@campus.lmu.de>
2023-01-28 13:01:19 +0100

    :clown_face: add mock for comments api

--- react-frontend/src/api/index.ts
+++ react-frontend/src/api/index.ts
@@ -1,6 +1,7 @@
+import {comments} from './comments';
 import {memes} from './memes';
 import {my} from './my';
 import {templates} from "./templates";
 
-export const api = {memes, my, templates};
+export const api = {comments, memes, my, templates};
 
