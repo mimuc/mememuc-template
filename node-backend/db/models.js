46f6e1773d3d7374f06f3e9c08d4a8d6681f5c92
Marcel Quanz <marcel.quanz@gmail.com>
2023-01-31 13:58:15 +0100

    Templates now also have a url virtual property.

--- node-backend/db/models.js
+++ node-backend/db/models.js
@@ -1,8 +1,5 @@
 const mongoose = require('mongoose');
 
-// TODO: Implement auth
-// TODO: Change url to $id, map url in response to proper url, get rid of image buffer in responses
-// TODO: Implement like/unlike on memes
 // TODO: Implement comment GET/POST
 
 const url = `http://localhost:3001/resources/images/`; // FIXME:
@@ -50,8 +47,17 @@ const Template = mongoose.model('Template', new mongoose.Schema({
         contentType: { type: String, default: 'image/png' }, // TODO: Make enum
         createdAt: { type: Date, default: Date.now },
         texts: { type: Array }
+    }, 
+    {
+        id: false,
+        toObject: { virtuals: true },
+        toJSON: { virtuals: true }
     })
 );
+
+Template.schema.virtual('url').get(function() {
+    return `${url}${this.publicId}`;
+});
 
 const Comment = mongoose.model('Comment', new mongoose.Schema({
     content: { type: String, required: true, unique: true  },
