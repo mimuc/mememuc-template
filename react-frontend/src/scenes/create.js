5a6316a080f2afd74b2cb3e6da1e8017be1a9acd
Totta-Git <tambato.carlotta@gmail.com>
2023-02-22 00:33:52 +0100

    Image upload from device, Image upload from URL, Take a Picture from device

--- react-frontend/src/scenes/create.js
+++ react-frontend/src/scenes/create.js
@@ -1,10 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import Meme from "../components/meme";
 import { getAllMemes } from '../api/memes';
+import { useNavigate } from "react-router-dom";
+import Button from 'react-bootstrap/Button';
 
-// Create component to upload user's image and display existing memes
-export default function Create() {
+export default function Create(props) {
   const [data, setData] = useState([]);
+  const [image, setImage] = useState(null);
+  const [imageUrl, setImageUrl] = useState("");
+  const navigate = useNavigate();
 
   // Get all memes from the used API
   useEffect(() => {
@@ -20,16 +24,61 @@ export default function Create() {
     });
   }, []);
 
+  const handleImageUpload = (e) => {
+    const file = e.target.files[0];
+    setImage(file);
+  };
+
+  const handleImageUrlChange = (e) => {
+    setImageUrl(e.target.value);
+  }
+
+  const handleCreate = (e) => {
+    e.preventDefault();
+    if (image) {
+      const formData = new FormData();
+      formData.append('file', image);
+      const url = URL.createObjectURL(image);
+      navigate(`/editor?url=${encodeURIComponent(url)}`);
+    } else if (imageUrl) {
+      navigate(`/editor?url=${encodeURIComponent(imageUrl)}`);
+    }
+  }
+
+  const handleCameraClick = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+      const video = document.createElement('video');
+      const canvas = document.createElement('canvas');
+      video.srcObject = stream;
+      video.onloadedmetadata = () => {
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+        canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+        const dataURI = canvas.toDataURL('image/png');
+        setImageUrl(dataURI);
+        stream.getTracks().forEach(track => track.stop());
+      };
+      video.play();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <>
       <h1> Create </h1>
+      <div>
+        <form onSubmit={handleCreate}>
+          <input type="file" accept="image/*" onChange={handleImageUpload} />
+          <input type="text" placeholder="Enter image URL" value={imageUrl} onChange={handleImageUrlChange} />
+          <Button type="submit" variant="primary" disabled={!image && !imageUrl}>Create</Button>
+          <Button onClick={handleCameraClick}>Take a Picture</Button>
+        </form>
+      </div>
       <div className="grid-container">
         {data.map(el => (<Meme img={el.url} title={el.name} width={el.width} height={el.height} />))}
       </div>
     </>
   );
 };
-
-
-
-
--- react-frontend/src/scenes/editor.js
+++ react-frontend/src/scenes/editor.js
@@ -1,27 +1,25 @@
 import React, { useState, createRef } from "react";
-import { useSearchParams } from "react-router-dom";
-import { exportComponentAsJPEG } from 'react-component-export-image';
+import { useLocation } from "react-router-dom";
 import Text from "../components/text";
 import Button from 'react-bootstrap/Button';
 
 // Editor component to create new memes
 export default function Editor() {
-    const [parameters] = useSearchParams();
     const [counter, setCounter] = useState(0);
     const inputText = () =>{ setCounter(counter + 1)};
     const memeRef = createRef();
-
+    const location = useLocation();
+    const imageUrl = decodeURIComponent(new URLSearchParams(location.search).get("url"));
 
     return ( 
         <>
             <h1> Editor </h1>
             <div style = {{width: " 250px", height: " 250px", border: '1px solid'}} ref = {memeRef} className = "meme mt-5 mb-5">
-                <img src={parameters.get("url")} width = "250px"/>
+                <img src={imageUrl} width = "250px"/>
                 { Array(counter).fill(0).map(e => <Text />) }
             </div>
                  
-            <Button onClick={inputText}> Add Text </Button> 
-            <Button variant="success" onClick={(e) => exportComponentAsJPEG(memeRef)}> Save </Button>     
+            <Button onClick={inputText}> Add Text </Button>      
         </>
     );  
 };
