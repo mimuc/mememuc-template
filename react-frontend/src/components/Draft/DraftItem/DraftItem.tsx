e7be3eb3c4e171bd9a5532229c887c23e2aeb053
Simon Lund <simon97lund@gmail.com>
2023-02-22 21:38:18 +0100

    :sparkles: implement draft

--- react-frontend/src/components/Draft/DraftItem/DraftItem.tsx
+++ react-frontend/src/components/Draft/DraftItem/DraftItem.tsx
@@ -1,5 +1,7 @@
-import {DraftType} from "src/types";
+import {DraftType, ImageShapeInterface} from "src/types";
 import {useDrafts} from "src/hooks";
+import {Button} from "antd";
+import {DeleteOutlined} from "@ant-design/icons";
 
 type DraftItemProps = {
     draft: DraftType
@@ -16,8 +18,33 @@ export const DraftItem = ({draft}: DraftItemProps) => {
         deleteDraft(draft.id)
     };
 
-    // TODO: display draft
-    // TODO: display basic info (number of texts and numebr of images)
-    // TODO: wire handlers
-    return null;
+    const firstImage = draft.shapes.find(s => s.type === 'image') as ImageShapeInterface;
+    const numberImages = draft.shapes.filter(s => s.type === 'image').length;
+    const numberTexts = draft.shapes.filter(s => s.type === 'text').length;
+
+    return (
+        <div style={{
+            display: 'inline-block',
+            width: 200,
+            marginRight: 20,
+        }}>
+            <div style={{
+                marginBottom: 20,
+                width: 200,
+                height: 200,
+                backgroundColor: 'lightgray',
+                borderRadius: 10,
+            }} onClick={handleLoad}>
+                {firstImage &&
+                    <img src={firstImage.url} style={{objectFit: 'contain', width: '100%', height: '100%'}}
+                         alt={'TemplateItem Image'}/>}
+            </div>
+            <div style={{display: 'flex', alignItems: 'center', justifyContent: 'space-between'}}>
+                <span style={{color: 'darkslategray'}}>
+                    {numberImages} image{numberImages != 1 ? 's' : ''}, {numberTexts} text{numberTexts != 1 ? 's' : ''}
+                </span>
+                <Button icon={<DeleteOutlined/>} onClick={handleDelete} danger/>
+            </div>
+        </div>
+    );
 }
