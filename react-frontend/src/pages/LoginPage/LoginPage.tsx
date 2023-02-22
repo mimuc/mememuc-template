7153d4052f7f771fdf8f8613b8fdcf5e4d3a9357
Marcel Quanz <marcel.quanz@gmail.com>
2023-02-22 19:00:04 +0100

    Working on the login page.

--- react-frontend/src/pages/LoginPage/LoginPage.tsx
+++ react-frontend/src/pages/LoginPage/LoginPage.tsx
@@ -1,25 +1,54 @@
+import Cookies from 'js-cookie';
+import bcrypt from 'bcryptjs';
+import { useState } from 'react';
 import {Button, Col, Form, Input, Row, theme, Typography} from "antd";
-import {Link} from "react-router-dom";
+import {Link/* , useHistory */} from "react-router-dom";
 
 const {Title, Text} = Typography;
 
-export const LoginPage = () => {
-    const {token} = theme.useToken();
-    const handleFinish = (values: any) => {
-        console.log(values);
-    }
 
-    // TODO: allow to login with username
+
+export const LoginPage = () => {
+    const { token } = theme.useToken();
+    //const history = useHistory();
+    const [error, setError] = useState('');
+
+    const handleFinish = async (values: any) => {
+        console.log("LOgging in", values)
+        try {
+            const hashedPassword = await bcrypt.hash(values.password, 10);
+            console.log(hashedPassword)
+            const response = await fetch('http://localhost:3001/auth/login', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({username: values.username, password: hashedPassword}),
+            });
+            console.log(response)
+
+            if (!response.ok) {
+                const error = await response.text();
+                throw new Error(error);
+            }
+
+            const data = await response.json();
+            Cookies.set('token', data.token);
+            //history.push('/dashboard');
+        } catch (error) {
+            console.error(error);
+            //setError(error);
+        }
+    };
+
     return (
         <Row>
             <Col offset={8} span={6}>
                 <Title>Login</Title>
                 <Form name={'login'} onFinish={handleFinish}>
                     <Form.Item required>
-                        <Input placeholder={'Username'}/>
+                        <Input placeholder={'Username'} name="username" />
                     </Form.Item>
                     <Form.Item required>
-                        <Input placeholder={'Password'}/>
+                        <Input.Password placeholder={'Password'} name="password" />
                     </Form.Item>
                     <Form.Item>
                         <Button type={'primary'} htmlType={'submit'} block>
@@ -29,6 +58,11 @@ export const LoginPage = () => {
                             or <Link to={'/register'}>Register</Link>
                         </Text>
                     </Form.Item>
+                    {error && (
+                        <Text type="danger" style={{ display: 'block', textAlign: 'center' }}>
+                            {error}
+                        </Text>
+                    )}
                 </Form>
             </Col>
         </Row>
