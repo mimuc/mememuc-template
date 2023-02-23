0cea35097f31319a4ea97aec6070e0b7fd6d4697
Simon Lund <simon97lund@gmail.com>
2023-02-23 18:08:57 +0100

    :bug: fix types

--- react-frontend/src/api/auth.ts
+++ react-frontend/src/api/auth.ts
@@ -4,7 +4,7 @@ export const login = (username: string, password: string) => {
     return client.post('/auth/login', {
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({username, password})
-    }).then(res => res.data.json() as Promise<{token: string, expiryTime: string}>);
+    }).then(res => res.data as {token: string, expiryTime: string});
 }
 
 export const register = (username: string, displayName: string, password: string) => {
