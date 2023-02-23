c43c1700485d37d3287a1760a33d4c2c38e11c79
Simon Lund <simon97lund@gmail.com>
2023-02-23 17:43:16 +0100

    :sparkles: implement auth endpoint api

---
+++ react-frontend/src/api/auth.ts
@@ -0,0 +1,17 @@
+import {client} from "src/api/base";
+
+export const login = (username: string, password: string) => {
+    return client.post('/auth/login', {
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({username, password})
+    });
+}
+
+export const register = (username: string, displayName: string, password: string) => {
+    return client.post('/auth/register', {
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({username, displayName, password})
+    });
+}
+
+export const auth = {login, register};
--- react-frontend/src/api/index.ts
+++ react-frontend/src/api/index.ts
@@ -2,6 +2,7 @@ import {comments} from './comments';
 import {memes} from './memes';
 import {my} from './my';
 import {templates} from "./templates";
+import {auth} from "src/api/auth";
 
-export const api = {comments, memes, my, templates};
+export const api = {auth, comments, memes, my, templates};
 
