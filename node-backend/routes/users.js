af19f0fac7a7a0561ffa2fb44478ce47b3541d7f
sast5859 <sami.strack@gmail.com>
2023-02-23 22:08:58 +0100

    login and signup done

--- node-backend/routes/users.js
+++ node-backend/routes/users.js
@@ -5,16 +5,40 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   const db = req.db;
   const users = db.get('users');
-  users.find({username: req.query.username},{ projection: {basicauthtoken: 0} }) // return all user properties, except the basic auth token
+  users.find({connexion_name: req.query.connexion_name}) // return all user properties, except the basic auth token
+      .then((docs) => 
+      {
+        if(docs[0]){
+      res.json(docs[0])
+        }else(
+          res.status(500).send()
+        )
+      } )
+      .catch((e) => res.status(500).send())
+});
+
+/* look if username is in database  */
+router.get('/lookinto', function(req, res, next) {
+  const db = req.db;
+  const users = db.get('users');
+  users.find({connexion_name: req.query.username},{ projection: {basicauthtoken: 0} }) // return all user properties, except the basic auth token
       .then((docs) => res.json(docs[0]))
       .catch((e) => res.status(500).send())
 });
 
 router.post('/create', function(req, res, next) {
   const db = req.db;
-  const name = req.query.name;
+  const name = req.query.username;
   const users = db.get('users');
-  users.post({username: name}) // return all user properties, except the basic auth token
+  const model = {
+    connexion_name: name,
+    comments:[],
+    posts: [],
+    likes:[],
+    dislikes:[]
+
+  }
+  users.insert(model) // return all user properties, except the basic auth token
       .then((result) => res.status(200).send("user created"))
       .catch((e) => res.status(500).send())
 });
--- react-frontend/src/callback/callback_user.js
+++ react-frontend/src/callback/callback_user.js
@@ -2,17 +2,14 @@ const localserv = "http:/localhost:27017"
 
 
 async function createUser(name ) {
-    try {
-        
-      const res = await fetch(localserv+`/posts/create?user=${name}`, {
+    console.log("in the createUser");
+    return await fetch(localserv+`/users/create?user=${name}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       }
-    });
-  
-    }
-    catch(error){
-      console.error(error);
-    }
+    })
+    .then ( res => res.json())
   }
+
+  module.exports = {createUser};
