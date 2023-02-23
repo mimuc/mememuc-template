3b2ac06b7681a19430074038eee80737c540cff4
Simon Lund <simon97lund@gmail.com>
2023-02-23 19:59:57 +0100

    :recycle: replace config with authConfig

--- react-frontend/src/api/my.ts
+++ react-frontend/src/api/my.ts
@@ -1,16 +1,8 @@
 import {MemeType, SessionType} from "src/types";
-import {client} from "src/api/base";
-import Cookies from "js-cookie";
-import {AxiosRequestConfig} from "axios";
+import {authConfig, client} from "src/api/base";
 
 const account = () => {
-    const config = {
-        headers: {
-            Authorization: `Bearer ${Cookies.get("token")}`,
-        }
-    } as AxiosRequestConfig
-
-    return client.get<SessionType>('/my', config).then(res => res.data);
+    return client.get<SessionType>('/my', authConfig()).then(res => res.data);
 }
 
 const memes = () => {
