e7941206187c46d731c438a17190bee3ec9ad304
Simon Lund <simon97lund@gmail.com>
2023-02-23 18:12:58 +0100

    :construction: wip

--- react-frontend/src/api/memes.ts
+++ react-frontend/src/api/memes.ts
@@ -1,118 +1,54 @@
 import {MemeType} from "src/types";
 import Cookies from 'js-cookie';
 import {client} from "./base";
+import {AxiosRequestConfig} from "axios";
 
-/* const mock = [
-    {
-        id: '1',
-        name: 'I\'m not a scientist, but I think we can all agree that...',
-        image: 'https://i.imgflip.com/768vkl.jpg',
-        totalComments: 100,
-        totalLikes: 6500,
-        totalDislikes: 1000,
-        vote: 0,
-        createdAt: '2022-11-11',
-        creator: {
-            id: '1',
-            displayName: 'John Doe'
-        }
-    },
-    {
-        id: '2',
-        name: 'I\'m not sure if this is a good idea, but I\'m going to do it anyway',
-        image: 'https://i.imgflip.com/768vkl.jpg',
-        totalComments: 1100000,
-        totalLikes: 6300,
-        totalDislikes: 10000,
-        vote: -1,
-        createdAt: '2023-01-06',
-    },
-    {
-        id: '3',
-        name: 'I\'m not sure what\'s going on, but I\'m pretty sure it\'s not good',
-        image: 'https://i.imgflip.com/768vkl.jpg',
-        totalComments: 2300,
-        totalLikes: 1300,
-        totalDislikes: 100,
-        createdAt: '2022-03-04',
-        vote: 1,
-        creator: {
-            id: '1',
-            displayName: 'John Doe'
-        }
-    },
-    {
-        id: '4',
-        name: 'Here come dat boi',
-        image: 'https://i.imgflip.com/768vkl.jpg',
-        totalComments: 1000,
-        totalLikes: 6000,
-        totalDislikes: 10000000,
-        vote: -1,
-        createdAt: '2022-03-04',
-    }
-] as MemeType[];
- */
+export const all = async () => {
 
-/* const all = () => {
-    return Promise.resolve(mock)
-}
- */
+    return Promise.resolve([]);
 
-export const all = () => {
-    // return client.get('/templates');
-    return Promise.resolve(client.get('http://localhost:3001/memes', {
-        headers: {
-            Authorization: `Bearer ${Cookies.get("token")}`,
-        }
-    })
-    .then(res => res.data));
+    // // return client.get('/templates');
+    // const memes = await client.get<any[]>('http://localhost:3001/memes', {
+    //     headers: {
+    //         Authorization: `Bearer ${Cookies.get("token")}`,
+    //     }
+    // } as AxiosRequestConfig).then(res => res.data);
 }
 
-/* const get = (memeId: string) => {
-    return Promise.resolve<MemeType>(mock.find(m => m.id === memeId) as MemeType)
-} */
+export const add = () => {
+    return Promise.resolve();
+    // TODO: and if logged in attribute to user
 
+}
 const get = (memeId: string) => {
+    return Promise.resolve<MemeType>({} as MemeType);
     // return client.get('/templates');
-    return Promise.resolve(client.get(`http://localhost:3001/memes/{memeId}`, {
-        headers: {
-            Authorization: `Bearer ${Cookies.get("token")}`,
-        }
-    })
-    .then(res => res.data));
+    // return Promise.resolve(client.get(`http://localhost:3001/memes/{memeId}`, {
+    //     headers: {
+    //         Authorization: `Bearer ${Cookies.get("token")}`,
+    //     }
+    // })
+    //     .then(res => res.data));
 }
 
-/* const getRandomMeme = () => {
-    return Promise.resolve<MemeType>(mock[0])
-} */
 
 const getRandomMeme = () => {
-    // return client.get('/templates');
-    return Promise.resolve(client.get('http://localhost:3001/memes?sort=random&limit=1', {
-        headers: {
-            Authorization: `Bearer ${Cookies.get("token")}`,
-        }
-    })
-    .then(res => res.data[0]));
+    return Promise.resolve<MemeType>({} as MemeType);
+    // // return client.get('/templates');
+    // return Promise.resolve(client.get('http://localhost:3001/memes?sort=random&limit=1', {
+    //     headers: {
+    //         Authorization: `Bearer ${Cookies.get("token")}`,
+    //     }
+    // })
+    //     .then(res => res.data[0]));
 }
 
 const upvote = (memeId: string) => {
-    return Promise.resolve(client.put(`http://localhost:3001/memes/${memeId}/like`, {}, {
-        headers: {
-            Authorization: `Bearer ${Cookies.get("token")}`,
-        }
-    })
-    .then(res => res.data));
+    return Promise.resolve()
 }
 
 const downvote = (memeId: string) => {
-    return Promise.resolve(client.put(`http://localhost:3001/memes/${memeId}/dislike`, {}, {
-        headers: {
-            Authorization: `Bearer ${Cookies.get("token")}`,
-        }
-    })
-    .then(res => res.data));
+    return Promise.resolve()
 }
 
-export const memes = {all, get, getRandomMeme,upvote, downvote};
+export const memes = {all, get, getRandomMeme, upvote, downvote};
