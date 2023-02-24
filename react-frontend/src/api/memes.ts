c9436c49bcdfef4227fe898954a3038f5db6db1a
Simon Lund <simon97lund@gmail.com>
2023-02-24 01:19:19 +0100

    :recycle: adapt to new sort and fix bug in getRandom

--- react-frontend/src/api/memes.ts
+++ react-frontend/src/api/memes.ts
@@ -1,20 +1,16 @@
 import {authConfig, client} from "./base";
 import {FilterType, MemeType} from "src/types";
 
-export const list = async (offset: number = 0, limit: number = 10, sort: 'latest' | 'popular' = 'latest', filter: FilterType = {creationDate: null}, search: string | null = null) => {
-    // TODO: use this instead of all
-    // TODO: convert filter Date.parse()
-    let ahhh: any = sort;
-    if(ahhh === 'latest') ahhh = 'newest';
+export const list = async (offset: number = 0, limit: number = 10, sort: 'newest' | 'oldest' = 'newest', filter: FilterType = {creationDate: null}, search: string | null = null) => {
     const queryList = [];
-    queryList.push(`limit=${limit}`, `sort=${ahhh}`, `skip=${offset}`);
-    if(filter?.creationDate) queryList.push(`after=${+filter.creationDate.valueOf()}`);
-    if(search) queryList.push(`name=${search}`);
+    queryList.push(`limit=${limit}`, `skip=${offset}`, `sort=${sort}`);
 
-    const query = `?${queryList.join('&')}`;
+    if (filter?.creationDate) queryList.push(`after=${+filter.creationDate.valueOf()}`);
+    if (search) queryList.push(`name=${search}`);
 
-    return client.get(`/memes${query}`, authConfig())
-    .then(res => res.data as MemeType[]);
+    const query = queryList.join('&');
+    return client.get(`/memes?${query}`, authConfig())
+        .then(res => res.data as MemeType[]);
 }
 
 export const add = async (store: "unlisted" | "private" | "public", memeName: string, image: string, width: number, height: number) => {
@@ -45,14 +41,20 @@ const get = async (memeId: string) => {
 
 
 const getRandomMeme = (filter: FilterType = {creationDate: null}, search: string | null = null) => {
-    // TODO: convert filter Date.parse()
-    return client.get('/memes?sort=random&limit=1', authConfig())
+    const queryList = [];
+    queryList.push('sort=random', 'limit=1');
+
+    if (filter?.creationDate) queryList.push(`after=${+filter.creationDate.valueOf()}`);
+    if (search) queryList.push(`name=${search}`);
+
+    const query = queryList.join('&');
+    return client.get(`/memes?${query}`, authConfig())
         .then(res => res.data[0] as MemeType);
 }
 
 const upvote = (memeId: string) => {
     return Promise.resolve(client.put(`/memes/${memeId}/like`, {}, authConfig())
-        .then(res => res.data));
+        .then(res => res.data as MemeType[]));
 }
 
 const upvoteRemove = (memeId: string) => {
