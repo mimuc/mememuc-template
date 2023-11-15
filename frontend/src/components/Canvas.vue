e55e80ed7722b8b9a34f3de69d844085d5491009
Eduard von Briesen <e.v.briesen@googlemail.com>
2023-11-15 19:13:40 +0100

    feat(frontend): ✨ basic meme editor

--- frontend/src/components/Canvas.vue
+++ frontend/src/components/Canvas.vue
@@ -1,22 +1,35 @@
-<script setup>
+<script setup lang="ts">
 import { fabric } from "fabric";
 import { onMounted, ref } from "vue";
+import runningAwayBalloon from "../assets/templates/running_away_ballon.jpeg";
 
 const can = ref(null);
+const texts: any = ref([]);
+let canvas: fabric.Canvas;
 
 onMounted(() => {
-  const canvas = new fabric.Canvas(can.value);
-  const rect = new fabric.Rect({
-    fill: "red",
-    width: 20,
-    height: 20,
-  });
-  canvas.add(rect);
+  canvas = new fabric.Canvas(can.value);
+  new fabric.Image.fromURL(
+    runningAwayBalloon,
+    (img: { width: any; height: any }) => {
+      canvas.setWidth(img.width);
+      canvas.setHeight(img.height);
+      canvas.setBackgroundImage(img, canvas.renderAll.bind(canvas));
+    },
+  );
 });
+
+function addText() {
+  console.log("add text");
+  const text = new fabric.IText("hello world", { left: 100, top: 100 });
+  canvas.add(text);
+  texts.value.push(text);
+}
 </script>
 
 <template>
   <div class="border-primary w-fit border-spacing-1 border-2">
     <canvas ref="can" width="500" height="500"></canvas>
   </div>
+  <button class="btn btn-primary" @click="addText">Add Text</button>
 </template>
