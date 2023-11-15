8539723ab777d124d110e49416c24606acebb0c3
Eduard von Briesen <e.v.briesen@googlemail.com>
2023-11-15 18:20:57 +0100

    chore(frontend): 🎉 initialized fabric canvas

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
+import Canvas from "./components/Canvas.vue";
 </script>
 
 <template>
-  <div class="prose">
-    <h1>Meme Muc</h1>
-    <p>
-      This is a meme generator for the Online Multimedia lecture at LMU Munich.
-    </p>
+  <div class="flex w-full flex-col items-center gap-4 p-16">
+    <div class="prose">
+      <h1>Meme Muc</h1>
+      <p>
+        This is a meme generator for the Online Multimedia lecture at LMU
+        Munich.
+      </p>
+    </div>
+    <Canvas />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
---
+++ frontend/src/components/Canvas.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { fabric } from "fabric";
+import { onMounted, ref } from "vue";
+
+const can = ref(null);
+
+onMounted(() => {
+  const canvas = new fabric.Canvas(can.value);
+  const rect = new fabric.Rect({
+    fill: "red",
+    width: 20,
+    height: 20,
+  });
+  canvas.add(rect);
+});
+</script>
+
+<template>
+  <div class="border-primary w-fit border-spacing-1 border-2">
+    <canvas ref="can" width="500" height="500"></canvas>
+  </div>
+</template>
