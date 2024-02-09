1d5048f1a0a528124ddcd61f64342bb2a119f867
Laura.Garcia <99455921+lcocooo@users.noreply.github.com>
2024-02-09 14:42:50 +0100

    page title

--- react-frontend/src/Pages/Editor/EditorPage.jsx
+++ react-frontend/src/Pages/Editor/EditorPage.jsx
@@ -1,12 +1,23 @@
 import { Container, Row, Col } from 'react-bootstrap';
 
 function EditorPage() {
+
+
     return (
+
+        <div>
+            <h1>This is the Editor</h1>
+        </div>
+
+        /*
+        // Paul's code commented out
         <Container fluid className="vh-100">
             <Row className="h-100">
                 <Col className="bg-coral d-flex align-items-center justify-content-center">Editor is displayed here</Col>
             </Row>
         </Container>
+
+         */
     );
 }
 
